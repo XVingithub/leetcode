a8052ead15d539188f8fd8739a07b5a144399d8b
xuwei <xuwei@songguo7.com>
2019-09-10 14:36:30 +0800

    add qianxin

--- src/main/java/qianxin/Main.java
+++ src/main/java/qianxin/Main.java
@@ -1,4 +1,72 @@
 package qianxin;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.Scanner;
+
+/**
+ * 输入二叉查找树，查找两个节点的最近公共祖先。
+ */
+class TreeNode1 {
+
+    TreeNode1 left;
+    TreeNode1 right;
+    int val;
+    TreeNode1(int x){
+        this.val=x;
+    }
+
+}
 public class Main {
+    public static TreeNode1 createNode(TreeNode1 rot,int index,int[] a) {
+        if(index>= a.length ) {
+            return null;
+        }
+        if(a[index]==-1) {
+            return null;
+        }
+        rot=new TreeNode1(a[index]);
+        rot.left=createNode(rot.left,2*index+1,a);
+        rot.right=createNode(rot.right,2*index+2,a);
+        return rot;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner=new Scanner(System.in);
+        int x=scanner.nextInt();
+        int n=1<<x;
+        n=n-1;
+        int[] a=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=scanner.nextInt();
+        }
+        TreeNode1 head=new TreeNode1(-1);
+
+
+        head=createNode(head,0,a);
+        int left=scanner.nextInt();
+        int right=scanner.nextInt();
+        TreeNode1 res=getFather(head,left,right);
+        System.out.println(res.val);
+    }
+
+
+
+    public static TreeNode1 getFather(TreeNode1 root,int x,int y){
+        if(root==null)
+            return null;
+        if(root.val==x||root.val==y)
+            return root;
+        TreeNode1 left=getFather(root.left,x,y);
+        TreeNode1 right=getFather(root.right,x,y);
+        if(left!=null&&right!=null)
+            return root;
+        if(left!=null){
+            return left;
+        }else {
+            return right;
+        }
+    }
+
 }
--- src/main/java/qianxin/ProcessTree.java
+++ src/main/java/qianxin/ProcessTree.java
@@ -1,4 +1,84 @@
 package qianxin;
 
+import java.util.*;
+
+/**
+ * 给定n个进程，这些进程满足以下条件：
+ * 1 每个进程都有唯一的PID，其中PID为进程ID
+ * 2 每个进程最多只有一个父进程，但可能有多个子进程，用PPID表示父进程ID
+ * 3 若一个进程没有父进程，则其PPID为0
+ * 4 PID，PPID都是无符号整数
+ * 结束进程树的含义是结束一个进程时，它所有的子进程都会被结束
+ * 结束进程时结束总的进程数量
+ */
+class TreeNode {
+    int ob;
+    List<TreeNode> treeNodes = new LinkedList<>();
+
+    public TreeNode(int obj) {
+        this.ob = obj;
+    }
+}
+
 public class ProcessTree {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String pid = scanner.nextLine();
+        String ppid = scanner.nextLine();
+
+
+        String[] pids = pid.split(" ");
+        String[] ppids = ppid.split(" ");
+
+        int[] pidData = new int[pids.length];
+        int[] ppidData = new int[ppids.length];
+        int k = scanner.nextInt();
+        int sum = 0;
+
+
+        for (int i = 0; i < pidData.length; i++) {
+            pidData[i] = Integer.valueOf(pids[i]);
+            ppidData[i] = Integer.valueOf(ppids[i]);
+        }
+
+
+        Map<Integer, TreeNode> map = new HashMap<>();
+        for (int i = 0; i < ppids.length; i++) {
+            TreeNode treeNode;
+            int val = ppidData[i];
+            if (map.get(val) == null) {
+                treeNode = new TreeNode(val);
+                map.put(val, treeNode);
+            } else {
+                treeNode = map.get(val);
+            }
+            TreeNode child;
+            int childVal = pidData[i];
+            if (map.get(childVal) == null) {
+                child = new TreeNode(childVal);
+                map.put(childVal, child);
+            } else {
+                child = map.get(childVal);
+            }
+            treeNode.treeNodes.add(child);
+        }
+
+        TreeNode kNode = map.get(k);
+        sum = breadthFirst(kNode);
+        System.out.println(sum);
+    }
+
+    public static int breadthFirst(TreeNode root) {
+        TreeNode node = root;
+        Deque<TreeNode> treeNodes = new LinkedList<>();
+
+        int sum = 0;
+        treeNodes.add(node);
+        while (!treeNodes.isEmpty()) {
+            node = treeNodes.pop();
+            sum++;
+            treeNodes.addAll(node.treeNodes);
+        }
+        return sum;
+    }
 }
